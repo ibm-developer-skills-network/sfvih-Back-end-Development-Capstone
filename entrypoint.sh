b476e22f1065a4956a30fd27c363b25dc7bf3fd8
CF <captainfedora@skillup.tech>
2023-02-26 18:10:08 -0800

    adding superuser by default to entrypoint - move to env variables later

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,10 @@
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_PASSWORD=admin \
+DJANGO_SUPERUSER_EMAIL="admin@admin.com" \
+python manage.py createsuperuser --noinput
+
 exec "$@"
